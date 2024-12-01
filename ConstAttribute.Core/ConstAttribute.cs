18caf6b91b6e28569c24b8ffdd11a7ec618d17ae
Francesco Granozio <59570638+Francesco-Granozio@users.noreply.github.com>
2024-12-01 15:31:45 +0100

    fixed namespace and package problems

---
+++ ConstAttribute.Core/ConstAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+
+[AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+public sealed class ConstAttribute : Attribute { }
+
+
---
+++ ConstAttribute.Core/tools/README.txt
@@ -0,0 +1,3 @@
+#these scripts are safe and have been taken in reference to the microsoft site
+#https://learn.microsoft.com/en-us/nuget/guides/analyzers-conventions
+
